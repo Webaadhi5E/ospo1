f8737acda1f2617cc0c7bd8159691c2c6dec2e42
R.Adhithya <33666474+radhi1991@users.noreply.github.com>
2025-02-25 15:03:20 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,25 +1,40 @@
-const isProd = process.env.NODE_ENV === "production";
-const repoName = "ospo1"; // Update with your GitHub repository name
+const webpack = require('webpack');
+
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  assetPrefix: isProd ? `/${repoName}` : "",
+  output: "export", // Required for static exports like GitHub Pages
+  assetPrefix: isProd ? "." : "", // Ensures relative asset loading
+  trailingSlash: true, // Ensures consistency in static exports
   images: {
-    unoptimized: true, // Disable Next.js image optimization for GitHub Pages
+    unoptimized: true, // Fixes next/image issues on GitHub Pages
+  },
+  cssModules: true,
+  cssLoaderOptions: {
+    importLoaders: 1,
+    localIdentName: '[local]___[hash:base64:5]',
+    url: false,
   },
   webpack: (config) => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.ASSET_PREFIX': JSON.stringify(isProd ? "." : ""),
+      })
+    );
+
     config.resolve.modules.push(__dirname);
+
     config.module.rules.push({
       test: /\.svg$/,
       use: [
-        { loader: "babel-loader" },
-        {
-          loader: "react-svg-loader",
-          options: { jsx: true },
-        },
+        { loader: 'babel-loader' },
+        { loader: 'react-svg-loader', options: { jsx: true } },
       ],
     });
 
     return config;
   },
-  devIndicators: { autoPrerender: false },
+  devIndicators: {
+    autoPrerender: false,
+  },
 };
