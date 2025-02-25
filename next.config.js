dd25de048a98315e6e0f759a2557eecc6e7b5cba
R.Adhithya <33666474+radhi1991@users.noreply.github.com>
2025-02-25 16:13:56 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,49 +1,33 @@
 const webpack = require("webpack");
 
-const assetPrefix = "/";
-
 module.exports = {
   output: "export",
-  assetPrefix,
+  assetPrefix: "/",
   trailingSlash: true,
-
-  publicRuntimeConfig: {
-    assetPrefix,
+  future: {
+    webpack5: true, // Ensure Webpack 5 is used
   },
-
+  publicRuntimeConfig: {
+    assetPrefix: "/",
+  },
   images: {
     unoptimized: true,
   },
-
   cssModules: true,
   cssLoaderOptions: {
     importLoaders: 1,
     localIdentName: "[local]___[hash:base64:5]",
     url: false,
   },
-
-  webpack: (config, { isServer }) => {
+  webpack: (config) => {
     config.plugins.push(
       new webpack.DefinePlugin({
         "process.env.ASSET_PREFIX": JSON.stringify(""),
       })
     );
 
-    if (!isServer) {
-      config.resolve.fallback = { fs: false };
-    }
-
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        { loader: "babel-loader" },
-        { loader: "react-svg-loader", options: { jsx: true } },
-      ],
-    });
-
     return config;
   },
-
   devIndicators: {
     autoPrerender: false,
   },
