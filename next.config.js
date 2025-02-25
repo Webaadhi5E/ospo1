da929fd8a016543f24bf3c08d17c49261458b80f
R.Adhithya <33666474+radhi1991@users.noreply.github.com>
2025-02-25 16:03:34 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,18 @@
 const webpack = require("webpack");
-const isProd = process.env.NODE_ENV === "production";
 
-// Set the asset prefix correctly for production and local development
-const assetPrefix = isProd ? "/ospo1" : "";
+const assetPrefix = "/";
 
 module.exports = {
   output: "export",
   assetPrefix,
-  trailingSlash: true, // Ensures all static routes end with a `/`
-  
+  trailingSlash: true,
+
   publicRuntimeConfig: {
-    assetPrefix, // Expose assetPrefix to the client
+    assetPrefix,
   },
 
   images: {
-    unoptimized: true, // Fixes `next/image` for static exports
+    unoptimized: true,
   },
 
   cssModules: true,
@@ -25,19 +23,16 @@ module.exports = {
   },
 
   webpack: (config, { isServer }) => {
-    // Define ASSET_PREFIX globally
     config.plugins.push(
       new webpack.DefinePlugin({
-        "process.env.ASSET_PREFIX": JSON.stringify(assetPrefix),
+        "process.env.ASSET_PREFIX": JSON.stringify(""),
       })
     );
 
-    // Prevent `fs` module issues in client-side builds
     if (!isServer) {
       config.resolve.fallback = { fs: false };
     }
 
-    // SVG Loader for importing SVGs as React components
     config.module.rules.push({
       test: /\.svg$/,
       use: [
